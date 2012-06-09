08afc478ae9e63ce59cea8f374d9e7de97bcaf6d
Aaron Kromer <aaron.kromer@gmail.com>
2012-06-09 18:40:33 -0400

    Minor updates based on notes today.

--- region.erl
+++ region.erl
@@ -1,7 +1,25 @@
 -module(region).
 -compile(export_all).
 
--record(region, {neighbors, particles=[], particle_threshold, subdivision_counter}).
+-record(region,
+        { point = {0, 0},
+          side_length,
+          neighbors,
+          particles = [],
+          particle_threshold,
+          subdivision_counter }
+).
+
+loop() ->
+  receive
+    {move, Pid, {Axis, Sign}} ->
+      {_, XY} = find_particle(Pid),
+      NewXY = new_xy(XY, Axis, Sign),
+      % if NewXY in self then check internal_collision
+      %   if no collision, update particle coordinate
+      %   else notify_particles of collision
+      % else notify neighbor;
+  end.
 
 can_subdivide(Region) ->
   Region#region.subdivision_counter > 0.
@@ -11,3 +29,13 @@ need_to_subdivide(Region) ->
 
 collision_detected(Region) ->
   need_to_subdivide(Region) and not(can_subdivide(Region)).
+
+subdivide(Region) ->
+  % spawn four new processes
+  % 
+  % stuff
+  0.
+
+notify_neighbor_of_divide(Neighbor, NewRegions) ->
+  % stuff
+  0.
