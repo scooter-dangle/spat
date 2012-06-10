bfa927b2454b58a2419b53b68941d4210a9e0343
scooter-dangle <scottlsteele@gmail.com>
2012-06-09 21:08:59 -0400

    Implement find_particle and in_self.

--- region.erl
+++ region.erl
@@ -2,24 +2,37 @@
 -compile(export_all).
 
 -record(region,
-        { point = {0, 0},
+        { origin = {0, 0},
           side_length,
           neighbors,
           particles = [],
           particle_threshold,
-          subdivision_counter }
-).
+          subdivision_counter }).
 
-loop() ->
+loop(State) ->
   receive
-    {move, Pid, {Axis, Sign}} ->
-      {_, XY} = find_particle(Pid),
-      NewXY = new_xy(XY, Axis, Sign),
+    {move, Pid, Direction} ->
+      {_, XY} = find_particle(Pid, State#region.particles),
+      NewXY = new_xy(XY, Direction),
       % if NewXY in self then check internal_collision
       %   if no collision, update particle coordinate
       %   else notify_particles of collision
       % else notify neighbor;
   end.
+
+
+find_particle(Pid, Particles) ->
+  lists:keymap(Pid, 1, Particles).
+
+
+new_xy({X, Y}, {x, Sign}) -> {X + Sign, Y};
+new_xy({X, Y}, {y, Sign}) -> {X, Y + Sign}.
+
+
+in_self(NewXY={X, Y},
+        #region{point={OriginX, OriginY}, side_length=SideLength}) ->
+  (X < OriginX + SideLength andalso Y < OriginY + SideLength)
+    andalso (X >= OriginX andalso Y >= OriginY).
 
 can_subdivide(Region) ->
   Region#region.subdivision_counter > 0.
